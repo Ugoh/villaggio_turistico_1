77aebf8c66a28999d1de250f9704986eb74ce6f9
Lorenzo Godi <lorenzo.godi@outlook.it>
2017-12-05 16:45:39 +0100

    Aggiunta classe Connessione

---
+++ villaggio_turistico_1/Connessione.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Kone
+{
+    public class Connessione
+    {
+        private SqlConnection myConn;
+
+        public Connessione(string strConnessione)
+        {
+            myConn = new SqlConnection(strConnessione);
+        }
+
+        public SqlConnection GetConnessione() => myConn;
+
+        public string Apri()
+        {
+            try
+            {
+                myConn.Open();
+                return "ok";
+            }
+            catch (SqlException ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        public void Chiudi()
+        {
+            myConn.Close();
+        }
+    }
+}
